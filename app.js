da709aa45989c2d056d8bfc42ed4e8e47f81d265
comfortidy <comfortsunday108@gmail.com>
2022-11-15 05:29:07 +0100

    html,css,javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+var count = 0
+document.querySelector("#increase").addEventListener("click", () => {
+    changeCount(1);
+  });
+  
+  document.querySelector("#decrease").addEventListener("click", () => {
+    changeCount(-1);
+  });
+  
+  changeCount = (number) => {
+    count += number;
+    document.querySelector("#count").innerHTML = count;
+}
+
